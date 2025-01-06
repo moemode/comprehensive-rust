b98ddec3fd6ba9b3643c35272b5bb7229ebcd83f
moemode <pfeffer.moritz@gmail.com>
2025-01-06 20:53:37 +0000

    23 lifetimes

---
+++ crust/src/bin/23_lifetime_elision.rs
@@ -0,0 +1,26 @@
+#[derive(Debug)]
+struct Point(i32, i32);
+
+fn cab_distance(p1: &Point, p2: &Point) -> i32 {
+    (p1.0 - p2.0).abs() + (p1.1 - p2.1).abs()
+}
+
+fn nearest<'a>(points: &'a [Point], query: &Point) -> Option<&'a Point> {
+    let mut nearest = None;
+    for p in points {
+        if let Some((_, nearest_dist)) = nearest {
+            let dist = cab_distance(p, query);
+            if dist < nearest_dist {
+                nearest = Some((p, dist));
+            }
+        } else {
+            nearest = Some((p, cab_distance(p, query)));
+        };
+    }
+    nearest.map(|(p, _)| p)
+}
+
+fn main() {
+    let points = &[Point(1, 0), Point(1, 0), Point(-1, 0), Point(0, -1)];
+    println!("{:?}", nearest(points, &Point(0, 2)));
+}
